1a7cd4dbac07231f683e76e6299e868076dcefa1
Joadlaen <jackcullenmurphy1999@gmail.com>
2026-03-12 16:53:08 +0000

    create3d slat stuff and a blank basic level

---
+++ SlateStuff/Source/SlateStuff/SlateStuff.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SlateStuff.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SlateStuff, "SlateStuff" );
