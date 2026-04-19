62e41ecd6f73834223867cd1a7325fe33dcd2337
Joadlaen <jackcullenmurphy1999@gmail.com>
2026-04-19 23:01:05 +0100

    created the plugin project and started looking around the source code. will be looking into the blueprint code specifically soon

---
+++ blueprint print plugin/pluginProject/Source/pluginProject/pluginProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "pluginProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, pluginProject, "pluginProject" );
